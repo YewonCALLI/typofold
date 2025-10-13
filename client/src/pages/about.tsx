e0597e1a56a8907c7f214219c80b524a86855e05
YewonCALLI <96384200+YewonCALLI@users.noreply.github.com>
2025-10-14 04:10:38 +0900

    init

--- client/src/pages/about.tsx
+++ client/src/pages/about.tsx
@@ -1,54 +1,46 @@
 // src/pages/about.tsx
 
-import { useRouter } from 'next/router';
-import Head from 'next/head';
-import Image from 'next/image';
+import { useRouter } from 'next/router'
+import Head from 'next/head'
+import Image from 'next/image'
 
 export default function About() {
-  const router = useRouter();
+  const router = useRouter()
 
   return (
     <>
       <Head>
         <title>About - TypoFold</title>
-        <meta name="description" content="About TypoFold project" />
+        <meta name='description' content='About TypoFold project' />
       </Head>
 
-      <div className="aboutContainer">
-        <div className="header">
-          <button
-            className="title"
-            onClick={() => router.push('/')}
-          >
+      <div className='aboutContainer'>
+        <div className='header'>
+          <button className='title' onClick={() => router.push('/')}>
             TypoFold
           </button>
-          <button
-            className="aboutButton"
-            onClick={() => router.push('/')}
-          >
+          <button className='aboutButton' onClick={() => router.push('/')}>
             Go Back
           </button>
         </div>
-        
-        <div className="row">
+
+        <div className='row'>
           <div
             style={{
               width: '50%',
               height: 'fit-content',
               objectFit: 'contain',
-            }}
-          >
+            }}>
             <div
               style={{
                 padding: '56.25% 0 0 0',
                 position: 'relative',
                 backgroundColor: 'white',
-              }}
-            >
+              }}>
               <iframe
-                src="https://player.vimeo.com/video/1031770589?autoplay=1&loop=1&title=0&background=1"
-                frameBorder="0"
-                allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
+                src='https://player.vimeo.com/video/1031770589?autoplay=1&loop=1&title=0&background=1'
+                frameBorder='0'
+                allow='autoplay; fullscreen; picture-in-picture; clipboard-write'
                 style={{
                   position: 'absolute',
                   top: 0,
@@ -56,15 +48,15 @@ export default function About() {
                   width: '100%',
                   height: '100%',
                 }}
-                title="TypoFold"
+                title='TypoFold'
               />
             </div>
           </div>
-          
+
           <Image
-            src="/img/cute.png"
-            alt="group"
-            className="projectImage"
+            src='/img/cute.png'
+            alt='group'
+            className='projectImage'
             width={500}
             height={500}
             style={{
@@ -74,61 +66,43 @@ export default function About() {
             }}
           />
         </div>
-        
-        <div className="projectInfo">
-          <span className="projectTitle">About Project</span>
-          <p className="projectDescription">
-            {'<'}TypoFold{'>'} was designed to connect digital typography and
-            analog sculpture. The idea is to apply texture images created using
-            p5.js in a digital environment to a 3D typography model, and then
-            convert it into a paper blueprint to create a physical sculpture
-            that can be assembled in the real world. This is important because
-            it means that digital creations don't just stay on the screen, but
-            can actually be manifested in a form that can be touched and
-            assembled by hand. As a result of the project, users will be able to
-            print out textures and 3D typography directly from the web and
-            assemble them on paper, creating the same three-dimensional artwork
-            as their digital counterparts.
+
+        <div className='projectInfo'>
+          <span className='projectTitle'>About Project</span>
+          <p className='projectDescription'>
+            {'<'}TypoFold{'>'} was designed to connect digital typography and analog sculpture. The idea is to apply
+            texture images created using p5.js in a digital environment to a 3D typography model, and then convert it
+            into a paper blueprint to create a physical sculpture that can be assembled in the real world. This is
+            important because it means that digital creations don&apos;t just stay on the screen, but can actually be
+            manifested in a form that can be touched and assembled by hand. As a result of the project, users will be
+            able to print out textures and 3D typography directly from the web and assemble them on paper, creating the
+            same three-dimensional artwork as their digital counterparts.
           </p>
         </div>
-        
-        <div className="projectInfo">
-          <span className="projectTitle">
-            What inspired you to create your work?
-          </span>
-          <p className="projectDescription">
-            I was motivated by the fact that code-based design tools like p5.js
-            are not being applied to the design field outside of media arts. I
-            was trying to turn my previous work in p5.js into 3D merchandise,
-            and it was very difficult. Programs like Adobe's Illustrator make it
-            easy to create designs, but p5.js only allows you to capture
-            screenshots, so it took a lot of effort to convert them into
-            schematics.
+
+        <div className='projectInfo'>
+          <span className='projectTitle'>What inspired you to create your work?</span>
+          <p className='projectDescription'>
+            I was motivated by the fact that code-based design tools like p5.js are not being applied to the design
+            field outside of media arts. I was trying to turn my previous work in p5.js into 3D merchandise, and it was
+            very difficult. Programs like Adobe&apos;s Illustrator make it easy to create designs, but p5.js only allows
+            you to capture screenshots, so it took a lot of effort to convert them into schematics.
             <br />
             <br />
-            So I thought, "It would be great if there was a website where you
-            could put a 3D object on the website and it would automatically
-            create a schematic for you." I wanted to connect it with p5.js so
-            that you could create a 3D schematic of a graphic that changes in
-            real time.
+            So I thought, &quot;It would be great if there was a website where you could put a 3D object on the website
+            and it would automatically create a schematic for you.&quot; I wanted to connect it with p5.js so that you
+            could create a 3D schematic of a graphic that changes in real time.
             <br />
             <br />
-            For example, if you create an 'A' with typography, it will give you
-            the shape, add faces to it, make it 3D, and convert it into a fold.
-            The main reason I created {'<'}TypoFold{'>'} was because I thought 
-            it would be easy to use. I hope that people who see this project 
-            will be able to use it and have fun in the process.
+            For example, if you create an &apos;A&apos; with typography, it will give you the shape, add faces to it,
+            make it 3D, and convert it into a fold. The main reason I created {'<'}TypoFold{'>'} was because I thought
+            it would be easy to use. I hope that people who see this project will be able to use it and have fun in the
+            process.
           </p>
         </div>
-        
-        <Image 
-          src="/img/group.png" 
-          alt="group" 
-          className="projectImage"
-          width={1200}
-          height={800}
-        />
+
+        <Image src='/img/group.png' alt='group' className='projectImage' width={1200} height={800} />
       </div>
     </>
-  );
+  )
 }
